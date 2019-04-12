7d4d675a490c54ca177c378a0356c578785c3cab
FELUMA\henrique.simoes <henriquems@gmail.com>
2019-04-12 16:46:25 -0300

    Corrigindo server.js backend

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(cors());
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 io.on("connection", socket => {
    socket.on("connectRoom", box => {
@@ -32,4 +32,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 app.use(require("./routes"));
 
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
