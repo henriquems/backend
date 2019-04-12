f1d7eec09a9f66c4ed669f8b9c00f2c03d7f00ab
FELUMA\henrique.simoes <henriquems@gmail.com>
2019-04-12 16:30:46 -0300

    Corrigindo packjson

--- src/server.js
+++ src/server.js
@@ -10,10 +10,10 @@ app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-io.on('connection', socket => {
-   socket.on('connectRoom', box => {
+io.on("connection", socket => {
+   socket.on("connectRoom", box => {
        socket.join(box);
-   })
+   });
 });
 
 mongoose.connect('mongodb+srv://oministack:oministack@cluster0-hmpha.mongodb.net/oministack?retryWrites=true', {
